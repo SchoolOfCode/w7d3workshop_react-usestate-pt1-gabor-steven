571c7c71efbff0e854685011e5b2c7cea3a29e68
ghavasi <g.havasi@outlook.com>
2022-06-08 13:40:56 +0100

    Task 2 is completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from "react";
 import "./App.css";
 import { useState } from "react";
-import { bootcampers } from "./bootcampers.js";
+import { bootcampers, compliments } from "./bootcampers.js";
 
 function App() {
   const [bootcamperIndex, setBoootcamperIndex] = useState(0);
+  const [complimentIndex, setComplimentIndex] = useState(0);
 
   function handleClick() {
     let randomNumber = Math.floor(Math.random() * bootcampers.length);
     setBoootcamperIndex(randomNumber);
+    let secondNumber = Math.floor(Math.random() * compliments.length);
+    setComplimentIndex(secondNumber);
   }
 
   return (
     <div className="App">
       <h1>React useState</h1>
-      <p>{bootcampers[bootcamperIndex]}</p>
+      <p>
+        {bootcampers[bootcamperIndex]} {compliments[complimentIndex]}
+      </p>
       <button onClick={handleClick}>Click me for a random bootcamper!</button>
     </div>
   );
