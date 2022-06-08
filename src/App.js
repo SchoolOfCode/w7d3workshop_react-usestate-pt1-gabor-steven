f8b7fd7c570f6572b634d4aec960ed5391f8029f
Steven Cheung <stevencheung@hotmail.co.uk>
2022-06-08 12:23:40 +0100

    completed tasks 1.1-1.7

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
+import { bootcampers } from "./bootcampers.js";
 
 function App() {
+  const [bootcamperIndex, setBoootcamperIndex] = useState(0);
+
+  function handleClick() {
+    let randomNumber = Math.floor(Math.random() * bootcampers.length);
+    setBoootcamperIndex(randomNumber);
+  }
+
   return (
     <div className="App">
       <h1>React useState</h1>
-      <p>BOOTCAMPER NAME</p>
-      <button>Click me for a random bootcamper!</button>
+      <p>{bootcampers[bootcamperIndex]}</p>
+      <button onClick={handleClick}>Click me for a random bootcamper!</button>
     </div>
   );
 }
